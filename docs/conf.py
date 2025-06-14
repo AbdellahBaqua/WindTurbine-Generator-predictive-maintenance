e63543d10b7affa9bea4cf2518cc349a9f767548
Abdellahneedssome <baqua.abdellah@gmail.com>
2025-06-14 15:47:05 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme' # Read the Docs theme
-
+html_title = 'WindTurbinePredictive Maintenance Dashboard'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
